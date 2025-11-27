a95ca882499a7504c2402ed8316e9e530d4ebf24
TijsStegeman <tijsstegeman@hotmail.com>
2025-11-27 12:13:39 +0100

    Basis game

--- src/main/java/nl/saxion/game/Bloodspire/MainMenuScreen.java
+++ src/main/java/nl/saxion/game/Bloodspire/MainMenuScreen.java
@@ -5,9 +5,12 @@ import nl.saxion.gameapp.GameApp;
 import nl.saxion.gameapp.screens.ScalableGameScreen;
 
 public class MainMenuScreen extends ScalableGameScreen {
+    int selectedOption = 0;
+
     public MainMenuScreen() {
         super(1280, 720);
     }
+
 
     @Override
     public void show() {
@@ -19,19 +22,48 @@ public class MainMenuScreen extends ScalableGameScreen {
         super.render(delta);
 
         // When the user presses enter, go to the next screen
-        if (GameApp.isKeyJustPressed(Input.Keys.ENTER)) {
-            GameApp.switchScreen("BloodspireScreen");
+        if (GameApp.isKeyJustPressed(Input.Keys.SPACE) || GameApp.isKeyJustPressed(Input.Keys.ENTER)) {
+            if (selectedOption == 0) {
+                GameApp.switchScreen("MyLevelScreen");
+            } else if (selectedOption == 1) {
+                GameApp.quit();
+            }
+
+        }
+
+        // Select option
+        if (GameApp.isKeyJustPressed(Input.Keys.S) || GameApp.isKeyJustPressed(Input.Keys.DOWN)) {
+            selectedOption = (selectedOption + 1)%2;
+        }
+        if (GameApp.isKeyJustPressed(Input.Keys.W) || GameApp.isKeyJustPressed(Input.Keys.UP)) {
+            selectedOption = (selectedOption - 1)%2;
+            if (selectedOption < 0) {
+                selectedOption = 1;
+            }
         }
 
         // Render the main menu
         GameApp.clearScreen("black");
         GameApp.startSpriteRendering();
-        GameApp.drawTextCentered("basic", "Start Game (press enter)", getWorldWidth()/2, getWorldHeight()/2, "amber-500");
+        GameApp.drawTextCentered("Basic", "Bloodspire", getWorldWidth()/2, getWorldHeight()/2+100, "red-500" );
+        // Color selected item
+        if (selectedOption == 0) { //start
+            GameApp.drawTextCentered("basic", "Start", getWorldWidth()/2, getWorldHeight()/2, "amber-500");
+        } else {
+            GameApp.drawTextCentered("basic", "Start", getWorldWidth()/2, getWorldHeight()/2, "white");
+        }
+        if (selectedOption == 1) { //quit
+            GameApp.drawTextCentered("basic", "Quit", getWorldWidth()/2, getWorldHeight()/2-100, "amber-500");
+        } else {
+            GameApp.drawTextCentered("basic", "Quit", getWorldWidth()/2, getWorldHeight()/2-100, "white");
+        }
+        //GameApp.drawTextCentered("basic", "Start Game (press enter)", getWorldWidth()/2, getWorldHeight()/2, "amber-500");
         GameApp.endSpriteRendering();
     }
 
     @Override
     public void hide() {
+
         GameApp.disposeFont("basic");
     }
 }
---
+++ src/main/java/nl/saxion/game/Bloodspire/TestScreen.java
@@ -0,0 +1,30 @@
+package nl.saxion.game.Bloodspire;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import nl.saxion.gameapp.GameApp;
+import nl.saxion.gameapp.screens.ScalableGameScreen;
+
+public class TestScreen extends ScalableGameScreen {
+
+    public TestScreen() {
+        super(1280, 720);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}
+
