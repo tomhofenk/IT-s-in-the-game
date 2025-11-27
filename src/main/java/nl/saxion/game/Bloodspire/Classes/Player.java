02b5f90f0d7ec6573f2bd389f1aebfe9f6ef67f8
tomhofenk03 <485408@student.saxion.nl>
2025-11-27 13:05:24 +0100

    WIP

--- src/main/java/nl/saxion/game/Bloodspire/Classes/Player.java
+++ src/main/java/nl/saxion/game/Bloodspire/Classes/Player.java
@@ -4,7 +4,8 @@ package nl.saxion.game.Bloodspire.Classes;
 //class nog niet zeker of hij gebruikt kan worden, maar in ieder geval
 //een start richtlijn van de stats die de speler heeft
 public class Player {
-    double health = 100;
+    double hitpoints = 100;
+    double attackSpeed = 1;
     double attackDamage = 1;
     double defense = 0;
     double experience = 0;
---
+++ src/main/java/nl/saxion/game/Bloodspire/Classes/gear/Necklace.java
@@ -0,0 +1,10 @@
+package nl.saxion.game.Bloodspire.Classes.gear;
+
+public class Necklace {
+    String itemName = "";
+    String rarity = "";
+    double hitpointsValue = 0;
+    double defenseValue = 0;
+    double speedPenalty = 0;
+    int itemID = 600;
+}
