a42b55bd1acb64f39cf57ebb6198eb9bb4003ecb
TijsStegeman <tijsstegeman@hotmail.com>
2025-11-28 10:42:14 +0100

    gridmovement

--- src/main/java/nl/saxion/game/Bloodspire/Classes/Tile.java
+++ src/main/java/nl/saxion/game/Bloodspire/Classes/Tile.java
@@ -1,10 +1,10 @@
 package nl.saxion.game.Bloodspire.Classes;
 public class Tile {
-     boolean hasEnemy;
-     int enemyID = 999;
      int tileID = 0;
      int worldX = 0;
      int worldY = 0;
      int gridX = 0;
      int gridY = 0;
+    boolean hasEnemy = false;
+    int enemyID = 999;
 }
--- src/main/java/nl/saxion/game/Main.java
+++ src/main/java/nl/saxion/game/Main.java
@@ -7,10 +7,21 @@ import nl.saxion.gameapp.GameApp;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Variabels for size of world and viewport (for now only in MyLevelScreen)
+        // Amount of pixels per tile
+        int pixelsPerGridTile = 64; // 64 Because sprite is 64x64
+        // 16 Wide * 9 High viewport (16*9)*64
+        int viewportWidth = 16 *  pixelsPerGridTile;
+        int viewportHeight = 9 *  pixelsPerGridTile;
+        // 64 Wide * 64 High viewport (64*64)*64
+        int worldWidth = 64 *  pixelsPerGridTile;
+        int worldHeight = 64 *  pixelsPerGridTile;
+
         // Add screens
         GameApp.addScreen("MainMenuScreen", new MainMenuScreen());
         GameApp.addScreen("BloodspireScreen", new BloodspireScreen());
-        GameApp.addScreen("MyLevelScreen", new MyLevelScreen());
+        GameApp.addScreen("MyLevelScreen", new MyLevelScreen(viewportWidth, viewportHeight, worldWidth, worldHeight));
         GameApp.addScreen("TestScreen", new TestScreen());
         GameApp.addScreen("TestSreen2", new TestSreen2());
 
