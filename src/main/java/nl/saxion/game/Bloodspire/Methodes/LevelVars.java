21050701b62389fc85b1e94314b4ed36cd47251b
tomhofenk03 <485408@student.saxion.nl>
2026-01-08 14:03:16 +0100

    hihi oeps

--- src/main/java/nl/saxion/game/Bloodspire/Methodes/LevelVars.java
+++ src/main/java/nl/saxion/game/Bloodspire/Methodes/LevelVars.java
@@ -30,8 +30,6 @@ public class LevelVars {
 //    }
 
     public static int getLevelStartX(int level) {
-        System.out.println("DEZE SNAP IK NIET WANNEER RUNT DEZE");
-        System.out.println(levelStartX.get(2));
         return levelStartX.get(level);
     }
 //
