2ebc781512fb5548ab41459c9f6cf9e63abffe1a
TijsStegeman <tijsstegeman@hotmail.com>
2026-01-15 13:21:59 +0100

    In memory of Stout Woutje

--- src/main/java/nl/saxion/game/Bloodspire/DooDScreen.java
+++ src/main/java/nl/saxion/game/Bloodspire/DooDScreen.java
@@ -2,6 +2,7 @@ package nl.saxion.game.Bloodspire;
 
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
+import nl.saxion.game.Main;
 import nl.saxion.gameapp.GameApp;
 import nl.saxion.gameapp.screens.ScalableGameScreen;
 
@@ -10,12 +11,15 @@ import java.awt.*;
 public class DooDScreen extends ScalableGameScreen {
 
     public DooDScreen() {
-        super(1600, 900);
+        super(8000, 4500);
     }
 
     @Override
     public void show() {
-        GameApp.addFont("Basic", "fonts/basic.ttf", 25);
+        GameApp.addFont("Basic", "fonts/basic.ttf", 1000);
+        GameApp.addFont("Basic2", "fonts/basic.ttf", 400);
+        GameApp.addTexture("StoutWoutje", "textures/TheAlmightyStoutWoutje.jpg");
+        Main.setMapAndEnemyData();
     }
 
     @Override
@@ -29,7 +33,11 @@ public class DooDScreen extends ScalableGameScreen {
 
         GameApp.clearScreen(Color.BLACK);
         GameApp.startSpriteRendering();
-        GameApp.drawTextCentered("Basic" ,"STOUT WOUTJE", getWorldWidth()/2, getWorldHeight()/2, Color.WHITE);
+        GameApp.drawTextCentered("Basic" ,"Defeated", getWorldWidth()/2, getWorldHeight()-500, Color.RED);
+        GameApp.drawTextCentered("Basic2", "By The Almighty Stout Woutje", getWorldWidth()/2, getWorldHeight()-750, Color.WHITE);
+        GameApp.drawTextureCentered("StoutWoutje", getWorldWidth()/2, getWorldHeight()/2);
+        GameApp.drawTextCentered("Basic2", "Press Escape to go to the main menu",  getWorldWidth()/2, getWorldHeight()-3400, Color.WHITE);
+        GameApp.drawTextCentered("Basic2", "Press Enter to start over",  getWorldWidth()/2, getWorldHeight()-3550, Color.WHITE);
         GameApp.endSpriteRendering();
     }
 
